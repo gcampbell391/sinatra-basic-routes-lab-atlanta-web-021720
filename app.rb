7f1e76ea9172ba6435f901f15a6bcdcf4ac1d3ca
gcampbell391 <gcampbell391@gmail.com>
2020-03-08 22:00:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Gene Campbell III"
+    end
+
+    get '/hometown' do 
+        "My hometown is Woodstock, Ga. Yeee Yeee"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Lil Uzi's whole new album Eternal Atake"
+    end
 end
